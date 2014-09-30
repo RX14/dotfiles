95104894b1dc8e3e264db297d5ba4bc6e1f7c3e3
RX14 <chris@rx14.co.uk>
2014-09-30 21:31:01 +0100

    Minor setupWorkspace.sh changes. Includes loading xmodmap.

--- .i3/setupWorkspace.sh
+++ .i3/setupWorkspace.sh
@@ -1,21 +1,23 @@
+xmodmap ~/.Xmodmap
+
 sleep 1
 
 i3-msg 'workspace "4: dev2"; layout tabbed'
 background i3-sensible-terminal
-wait-for-window Terminal
+sleep 0.5
 
 i3-msg 'workspace "5: dev1"; layout tabbed'
 background i3-sensible-terminal
 sleep 0.5
 
 i3-msg 'workspace "1: web1"'
-background chromium-browser
+background chromium
 wait-for-window Chromium
 #wait-for-window "Tabs Outliner"
 #i3-msg '[class="Chromium-browser" instance="crx_eggkanocgddhmamlbiijnphhppkpkmkl"] focus; move left; resize shrink width 1000 px or 36 ppt'
 
 i3-msg 'workspace "2: web2"'
-background chromium-browser
+background chromium
 sleep 2
 
 i3-msg 'workspace "3: other"; layout tabbed'
@@ -27,4 +29,4 @@ background lightscreen
 background dropbox start
 background bluedevil-monolithic
 background skype
-background kmix
+background pasystray
