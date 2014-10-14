00fd2b599bab107f925e950a034d54b416169321
RX14 <chris@rx14.co.uk>
2014-10-14 18:31:08 +0100

    Major rework: IT ACTUALLY WORKS!

--- .i3/setupWorkspace.sh
+++ .i3/setupWorkspace.sh
@@ -4,15 +4,6 @@ sleep 1
 
 xmodmap /home/rx14/.Xmodmap
 
-i3-msg 'workspace "4: dev2"; layout tabbed'
-sleep 10
-background i3-sensible-terminal
-wait-for-window "None"
-
-i3-msg 'workspace "5: dev1"; layout tabbed'
-background i3-sensible-terminal
-sleep 5
-
 i3-msg 'workspace "1: web1"'
 background chromium
 wait-for-window Chromium
@@ -21,21 +12,28 @@ i3-msg 'workspace "2: web2"'
 background chromium
 sleep 5
 
-i3-msg 'workspace "6: mail"'
-background thunderbird
-wait-for-window Thunderbird
+i3-msg 'workspace "4: dev2"'
+background roxterm
+wait-for-window ROXTerm
+i3-msg '[class="Roxterm"] move container to workspace "4: dev2"'
 
-i3-msg 'workspace "3: other"; layout tabbed'
+i3-msg 'workspace "5: dev1"; for_window [class="Roxterm"] move container to workspace "5: dev1"'
+background roxterm
+sleep 5
+
 background steam
 background spotify
 background hexchat
 background lightscreen
+background skype
 background dropboxd start
 background bluedevil-monolithic
-background skype
 background pasystray
+background thunderbird
 
-for a  in 1 2 3 4 5 6; do
+COUNTER=0
+while [  $COUNTER -lt 240 ]; do
     xmodmap /home/rx14/.Xmodmap
-    sleep 10
+    sleep 0.5
+    let COUNTER=COUNTER+1
 done
