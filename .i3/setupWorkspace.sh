b5703a57bd83353089ded103d4c8809a6b951874
RX14 <chris@rx14.co.uk>
2014-10-27 11:56:44 +0000

    Switch around workspace ordering and numbering

--- .i3/setupWorkspace.sh
+++ .i3/setupWorkspace.sh
@@ -8,14 +8,14 @@ i3-msg 'workspace "1: web1"'
 background chromium
 wait-for-window Chromium
 
-i3-msg 'workspace "2: web2"'
+i3-msg 'workspace "4: web2"'
 background chromium
 sleep 5
 
-i3-msg 'workspace "4: dev2"'
+i3-msg 'workspace "2: dev2"'
 background roxterm
 wait-for-window ROXTerm
-i3-msg '[class="Roxterm"] move container to workspace "4: dev2"'
+i3-msg '[class="Roxterm"] move container to workspace "2: dev2"'
 
 i3-msg 'workspace "5: dev1"; for_window [class="Roxterm"] move container to workspace "5: dev1"'
 background roxterm
