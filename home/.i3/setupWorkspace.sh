79ba243e556ff03390384b2d8c89ab1f6220a2fb
RX14 <chris@rx14.co.uk>
2015-06-04 16:28:48 +0100

    Remove stuff for unneeded workspaces.

--- home/.i3/setupWorkspace.sh
+++ home/.i3/setupWorkspace.sh
@@ -12,27 +12,9 @@ i3-msg 'workspace "1: web1"'
 background chromium
 wait-for-window Chromium
 
-#i3-msg 'workspace "4: web2"'
-#background chromium
-#sleep 5
-
-i3-msg 'workspace "9: web3"'
-background chromium
-sleep 5
-
-i3-msg 'workspace "0: dev3"'
-background terminology
-wait-for-window Terminology
-sleep 2
-i3-msg '[class="terminology"] move container to workspace "0: dev3"'
-
-#i3-msg 'workspace "2: dev2"
-#background terminology
-#sleep 5
-
 i3-msg 'workspace "5: dev1"'
 background terminology
-sleep 5
+wait-for-window Terminology
 
 background lightscreen
 #background env LD_LIBRARY_PATH="" STEAM_RUNTIME=0 steam
