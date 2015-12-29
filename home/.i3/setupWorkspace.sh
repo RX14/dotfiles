30356b3757efec2b458f72d04673103ee36932f7
RX14 <chris@rx14.co.uk>
2015-12-29 10:47:35 +0000

    Fix workspace name

--- home/.i3/setupWorkspace.sh
+++ home/.i3/setupWorkspace.sh
@@ -16,7 +16,7 @@ wait-for-window "Mozilla Firefox"
 i3-msg 'workspace "2: dev"'
 background terminology
 wait-for-window Terminology
-i3-msg '[class="terminology"] move container to workspace "2: dev2"'
+i3-msg '[class="terminology"] move container to workspace "2: dev"'
 
 i3-msg 'workspace "3: other"'
 #background env LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1' steam
